3e7d8372d17939ff0f41b338d1040f267d3bfb95
App Academy Student <student@appacademy.io>
2016-09-29 12:15:06 -0700

    start PollApp project : create db and associations

---
+++ PollApp/db/migrate/20160929185212_create_responses.rb
@@ -0,0 +1,14 @@
+class CreateResponses < ActiveRecord::Migration
+  def change
+    create_table :responses do |t|
+      t.integer :user_id, null: false
+      t.integer :answer_id, null: false
+
+      t.timestamps null: false
+    end
+
+    add_index :responses, :user_id
+    add_index :responses, :answer_id
+    add_index :responses, [:user_id, :answer_id], unique: true
+  end
+end
